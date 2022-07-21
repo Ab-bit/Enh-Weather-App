ed74ce2e99a5186c5dc4b434c472de61f36c3b83
Abhishant Rai <66560032+Ab-bit@users.noreply.github.com>
2022-07-21 12:41:03 +0530

    Update Inputs.jsx

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -36,6 +36,7 @@ function Inputs({ setQuery, units, setUnits }) {
           onChange={(e) => setCity(e.currentTarget.value)}
           type="text"
           placeholder="Search for city...."
+          onClick={handleSearchClick}
           className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
         />
         <UilSearch
